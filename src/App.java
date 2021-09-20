516ba03936dbb844c9b76e3c0f6e30348098dde4
joebaa <81723153+joebaa@users.noreply.github.com>
2021-09-20 17:24:37 -0500

    temp push final

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class App {
+    private static Double tempc;
+
     public static void main(String[] args) throws Exception {
-        System.out.println("This program converts a temperature in degrees Celsius into a temperature in degrees Fahrenheit. Enter a temperature in degrees Celsius");
+        System.out.println("This program converts a temperature in degrees Celsius into a temperature in degrees Fahrenheit. Enter a temperature in Celsius");
+        System.out.print("Enter a temperature in degrees Celsius: ");
+        Scanner input = new Scanner(System.in);
+        tempc = input.nextDouble();
+        Double tempf = (9.0/5.0) * tempc + 32;
+        System.out.print(tempc + " degrees Celsius is equal to" + tempf + " degrees Fahrenheit. Goodbye...");
     }
 }
